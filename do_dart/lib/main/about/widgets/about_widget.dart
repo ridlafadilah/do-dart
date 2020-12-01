3027af82987d5d7883d9c7b17398016351505c23
Ridla Fadilah <ridla.fadilah@gmail.com>
2020-12-02 03:04:20 +0700

    dongkap

--- do_dart/lib/main/about/widgets/about_widget.dart
+++ do_dart/lib/main/about/widgets/about_widget.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:do_theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:package_info/package_info.dart';
 
 class AboutWidget extends StatefulWidget {
   const AboutWidget({Key key, this.animationController, this.animation})
@@ -14,6 +17,19 @@ class AboutWidget extends StatefulWidget {
 }
 
 class _AboutWidgetState extends State<AboutWidget> {
+  PackageInfo _packageInfo = PackageInfo(
+    appName: 'Unknown',
+    packageName: 'Unknown',
+    version: 'Unknown',
+    buildNumber: 'Unknown',
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _initPackageInfo();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -60,15 +76,15 @@ class _AboutWidgetState extends State<AboutWidget> {
           ListTile(
             leading: SvgPicture.asset(
                 'assets/eva_icons/outline/svg/star-outline.svg'),
-            title: const Text(
-              'Dongkap App',
-              style: TextStyle(fontFamily: AppTheme.fontName),
+            title: Text(
+              _packageInfo.appName,
+              style: const TextStyle(fontFamily: AppTheme.fontName),
             ),
             horizontalTitleGap: 2,
             contentPadding: const EdgeInsets.symmetric(horizontal: 20.0),
-            trailing: const Text(
-              '1.0.0',
-              style: TextStyle(fontFamily: AppTheme.fontName),
+            trailing: Text(
+              _packageInfo.version,
+              style: const TextStyle(fontFamily: AppTheme.fontName),
             ),
             onTap: () {},
           ),
@@ -80,5 +96,12 @@ class _AboutWidgetState extends State<AboutWidget> {
         ],
       ),
     );
+  }
+
+  Future<void> _initPackageInfo() async {
+    final PackageInfo info = await PackageInfo.fromPlatform();
+    setState(() {
+      _packageInfo = info;
+    });
   }
 }
